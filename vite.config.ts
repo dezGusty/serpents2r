4439aee52b3923289eb97d05b1ce3b73306b6c80
Augustin Preda <7242607+dezGusty@users.noreply.github.com>
2024-11-16 16:00:23 +0200

    (try-out) github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
-export default ({
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/serpents2r/',
   build: {
     target: 'esnext' //browsers can handle the latest ES features
   }
